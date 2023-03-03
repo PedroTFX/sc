0bbe8f720454c5df6a1379e0394419574da20d80
PedroTFX <pedro.trindade.tfx@gmail.com>
2023-03-03 16:55:03 +0000

    added class to read from userfile 	reads from file and puts in hashtable 	isAuthenticaded(String user, String password) // returns if the user and password match 	isRegisted(String user) // returns if the user is registed 	registerUser(String user, String password) // Registers user on HASTABLE ALL TESTED
    TODO: autheticate if the user already exists?
          register on the file?

---
+++ UserInfo.java
@@ -0,0 +1,83 @@
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.Hashtable;
+
+class UserInfo{
+
+    private static Hashtable<String, String> userTable;
+
+    /**
+     * Constructor that reads the file and puts in usertable
+     */
+    public UserInfo(String filename){
+        userTable = new Hashtable<String, String>();
+        try{
+            BufferedReader in = new BufferedReader(new FileReader(filename));
+            String line;
+            while((line = in.readLine()) != null){
+                String[] split = line.split(" ");
+                userTable.put(split[0], split[1]);
+            }
+            in.close();
+        }catch(IOException e){
+            System.err.println(e.getMessage());
+            System.exit(-1);
+        }
+    }
+
+    /**
+     * Returns if the user and the password are a match
+     * Assumes that the user is registered
+     */
+    public static boolean isAuthenticated(String user, String password){
+        return userTable.get(user).equals(password);
+    }
+
+    /**
+     * Returns if the user is registered
+     */
+    public static boolean isRegistered(String user){
+        return userTable.containsKey(user);
+    }
+
+    /**
+     * Registers a user on the hashtable
+     * 
+     * TODO: write to file ???
+     * If the user already exists do we authenticate or not?
+     * 
+     * @param user the username
+     * @param password the password
+     * @return true if the user was registered, false if the user already exists
+     */
+    public static boolean registerUser(String user, String password){
+        if(isRegistered(user)){
+            System.err.println("User already exists");
+            return false;
+        }
+        userTable.put(user, password);
+        //TODO: write to file ???
+        return true;
+    }
+
+
+    /**
+     * Tests
+     */
+    // public static void main(String[] args) {
+    //     //tests
+    //     UserInfo ui = new UserInfo("users.txt");
+    //     System.out.println(ui.isAuthenticated("user1", "pass1")); //true
+    //     System.out.println(ui.isAuthenticated("user1", "pass2")); //false
+    //     System.out.println(ui.isAuthenticated("user2", "pass2")); //true
+
+    //     System.out.println(ui.isRegistered("user1"));             //true
+    //     System.out.println(ui.isRegistered("user2"));             //true
+    //     System.out.println(ui.isRegistered("user3"));             //false
+
+    //     registerUser("user3", "pass3");
+    //     System.out.println(ui.isRegistered("user3"));             //true
+    //     System.out.println(ui.isAuthenticated("user3", "pass3")); //true
+    // }
+}
