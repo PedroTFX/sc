66036812441a33bc7b2d3561fec238729c98f352
56380 <joao.bf4@outlook.com>
2023-03-09 19:20:05 +0000

    Adicionadas as funcoes boughtWasWine classify
    parecem funcionar

--- Wine.java
+++ Wine.java
@@ -1,20 +1,58 @@
-import java.util.Hashtable;
+import java.io.IOException;
 
 public class Wine {
+	private static final int QUANTITY = 2;
+	private static final int BALANCE = 2;
+	private static final int VALUE = 1;
 	String name;
 	String image;
 	int stock;
 	int price;
-	Hashtable<User, Integer> reviews;
 
 	public Wine(String name, String image, String seller) {
 		this.name = name;
 		this.image = image;
 		stock = price = 0;
-		reviews = new Hashtable<User, Integer>();
 	}
 
 	public String toString() {
-		return String.format("%s | %s | %d | %d | %s", name, image, stock, price, reviews);
+		return String.format("%s | %s | %d | %d | %s", name, image, stock, price);
+	}
+
+	public static boolean boughtWasWine(String seller, String wine, int quantity) throws IOException {
+		String wineInfo = Data.readWineInfoFromFile(wine);
+		System.out.println(wineInfo);
+		String userInfo = Data.readUserInfoFromFile(seller);
+		System.out.println(userInfo);
+		String[] userTokens = userInfo.split(":");
+		String[] wineTokens = wineInfo.split(":");
+		wineTokens[QUANTITY] = String.valueOf(Integer.parseInt(wineTokens[QUANTITY]) - quantity);
+		userTokens[BALANCE] = String.valueOf(Integer.parseInt(userTokens[BALANCE]) - Integer.parseInt(wineTokens[VALUE]) * quantity);
+		String newWineLine = String.join(":", wineTokens);
+		String newUserLine = String.join(":", userTokens);
+		return Data.updateLineWines(wineInfo, newWineLine) && Data.updateLineUsers(userInfo, newUserLine);
+	}
+
+	public static boolean classify(String wine, int classification) throws IOException {
+		String info = Data.readWineInfoFromFile(wine);
+
+		String[] infoTokens = info.split(":");
+		infoTokens[infoTokens.length - 1] = infoTokens[infoTokens.length - 1] + "," + String.valueOf(classification);
+
+		String newLine = String.join(":", infoTokens);
+
+		return Data.updateLineWines(info, newLine);
+	}
+
+	public static void main(String[] args) throws IOException {
+/* 		boolean boughtWasWine = boughtWasWine("user", "porto", 1);
+		if (boughtWasWine) {
+			System.out.println("TRINTA!!!");
+		} */
+
+		boolean classify = classify("porto", 4);
+		if (classify) {
+			System.out.println("TRRRRRINTAAAAAAA");
+		}
 	}
 }
