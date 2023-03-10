daa2b53fac36e1961d288e08159dddfa9f867d28
56380 <joao.bf4@outlook.com>
2023-03-10 00:57:27 +0000

    Refactor no servidor  para aceitar a nova estrutura de processar pedidos (done (not tested))

--- TintolmarketServer.java
+++ TintolmarketServer.java
@@ -4,24 +4,14 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Hashtable;
-import java.util.Set;
 
 public class TintolmarketServer implements Serializable {
-
 	private static int port = 12345;
-	private static Hashtable<String, User> userTable;
-	private static ArrayList<Wine> wines;
 
 	public static void main(String[] args) {
 		System.out.println("servidor: main");
 		TintolmarketServer server = new TintolmarketServer();
-		userTable = new Hashtable<String, User>();
-		wines = new ArrayList<Wine>();
-		UserInfo userInfo = new UserInfo("users.txt");
-
 		// getting port from args else defaults to 1234
 		port = args.length > 0 ? Integer.parseInt(args[0]) : port;
 		server.startServer(port);
@@ -46,12 +36,11 @@ public class TintolmarketServer implements Serializable {
 			try {
 				Socket inSoc = sSoc.accept();
 				new ServerThread(inSoc);
-				int i = 0;
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		// sSoc.close();
+		//sSoc.close();
 	}
 
 	// Threads utilizadas para comunicacao com os clientes
@@ -106,11 +95,11 @@ public class TintolmarketServer implements Serializable {
 
 				}
 
-				// Check if user exists and password is correct
 
 				response.type = Response.Type.OK;
 				response.message = "OK";
-				if (!UserInfo.authentication(request.user, request.password)) {
+				// Check if user exists and password is correct
+				if (!Data.confirmPassword(userId, request.password)) {
 					response.type = Response.Type.ERROR;
 					response.message = "Combinação userID/password incorreta";
 					outStream.writeObject(response);
@@ -133,107 +122,70 @@ public class TintolmarketServer implements Serializable {
 				} catch (Exception e1) {
 					e1.printStackTrace();
 				}
-				/*
-				 * // TODO: refazer
-				 * // este codigo apenas exemplifica a comunicacao entre o cliente e o servidor
-				 * // nao faz qualquer tipo de autenticacao
-				 *
-				 * // checks if the user exists in the userTable, if it does, checks if the
-				 * // password is correct
-				 * // returns OK if the user exists and the password is correct, ERROR otherwise
-				 * if (request.operation == Request.Operation.AUTHENTICATE) {
-				 * if ((checkUser(request.user, request.password))) {
-				 * response.type = Response.Type.OK;
-				 * response.message = "hahha";
-				 * } else {
-				 * response.type = Response.Type.ERROR;
-				 * response.message = "NOT OK";
-				 * }
-				 * outStream.writeObject(response);
-				 * System.out.println("autentica");
-				 * }
-				 *
-				 * /*
-				 * try {
-				 * request = (Request) inStream.readObject();
-				 * // pass = (String) inStream.readObject();
-				 *
-				 * } catch (ClassNotFoundException e1) {
-				 * e1.printStackTrace();
-				 * }
-				 */
-
-				// TODO: loop between client and server for the actual communication
-				/*
-				 * if (request.operation == Request.Operation.WALLET) {
-				 * response.type = Response.Type.OK;
-				 * response.balance = 200;
-				 * outStream.writeObject(response);
-				 * System.out.println("responsi ao wallet");
-				 * } else {
-				 * response.type = Response.Type.ERROR;
-				 * response.balance = 199;
-				 * }
-				 * System.out.println("fim while");
-				 * request = null;
-				 * }
-				 */
-				System.out.println(userTable);
 			}
 		}
 
 		private Response processRequest(Request request) {
 			System.out.printf("REQUEST: %s\n", request);
 			Response response = new Response();
-			User user = userTable.get(userId);
-			Wine wine = null;
-			if (request.wine != null) {
-				wine = user.wineList.get(request.wine);
-			}
 			if (request.operation == Request.Operation.ADD) {
+				boolean newWine = Logic.addWine(userId, request.wine);
 				//User user = userTable.get(userId);
-				if (user.wineList.containsKey(request.wine)) {
+				if (!newWine){
 					response.type = Response.Type.ERROR;
 					response.message = "Esse vinho já existe!";
 				} else {
-					Wine newWine = new Wine(request.wine, request.image, userId);
-					user.wineList.put(request.wine, newWine);
-					wines.add(newWine);
 					response.type = Response.Type.OK;
 				}
 			} else if (request.operation == Request.Operation.SELL) {
-				//User user = userTable.get(userId);
-				//Wine wine = user.wineList.get(request.wine);
-				if (wine == null) {
+				boolean exists = Logic.sellWine(userId, request.wine, request.quantity);
+				if (!exists) {
 					response.type = Response.Type.ERROR;
 					response.message = "Esse vinho não existe!";
-				} else {
-					wine.stock -= request.quantity;
-					wine.price = request.value;
+				} else{
 					response.type = Response.Type.OK;
 				}
 			} else if (request.operation == Request.Operation.VIEW) {
-				if (wine == null) {
+				boolean exists = Logic.viewWine(request.wine);
+				if (!exists) {
 					response.type = Response.Type.ERROR;
 					response.message = "Este vinho nao existe";
 				}
-				response.averageWineClassification = averageWineClasification(wine.reviews);
+				response.seller = Logic.getSeller(request.wine);
+				response.averageWineClassification = Logic.averageWineClasification(request.wine);
 				response.type = Response.Type.OK;
 			} else if(request.operation == Request.Operation.WALLET){
 				response.type = Response.Type.OK;
-				response.balance = user.balance;
+				response.balance = Integer.parseInt(Logic.wallet());
+			} else if(request.operation == Request.Operation.CLASSIFY){
+				boolean reviewd = false;
+				try {
+					reviewd = Logic.classify(userId, request.stars);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				if(reviewd){
+					response.type = Response.Type.OK;
+				}
+			} else if(request.operation == Request.Operation.TALK){
+				boolean messageSent = Logic.sendMessage(userId, request.user, request.message);
+				if (messageSent) {
+					response.type = Response.Type.OK;
+				} else {
+					response.type = Response.Type.ERROR;
+					response.message = "Recetor não existe";
+				}
+			} else if(request.operation == Request.Operation.READ){
+				Hashtable<String, String[]> messages = Logic.getMessage(userId);
+				if (messages != null) {
+					response.messages = messages;
+					response.type = Response.Type.OK;
+				} else{
+					response.type = Response.Type.ERROR;
+				}
 			}
 			System.out.printf("RESPONSE: %s\n", response);
 			return response;
-		}
-
-		private int averageWineClasification(Hashtable<User, Integer> reviews) {
-			int sum = 0;
-			Set<User> userSet = reviews.keySet();
-			for (User user : userSet) {
-				sum += reviews.get(user);
-			}
-			return sum / userSet.size();
 		}
 
 		private void close() {
