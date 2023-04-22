0013b3f0c51e7cdb77a8b8fc518ac38a3b4dcd0c
56380 <joao.bf4@outlook.com>
2023-04-22 02:58:02 +0100

    Pequenas alteracoes a constantes

--- Constants.java
+++ Constants.java
@@ -1,10 +1,10 @@
 public class Constants {
-	public static final String IMAGES_FOLDER = "server-images\\";
+	public static final String SERVER_IMAGES_FOLDER = "server-images\\";
+	public static final String CLIENT_IMAGES_FOLDER = "client-images\\";
 	public static final String DB_FOLDER = "db";
 	public static final String USER_FILE = DB_FOLDER + "\\" + "users.txt";
 	public static final String WINE_FILE = DB_FOLDER + "\\" + "wines.txt";
-	public static final String SELLS_FILE = DB_FOLDER + "\\" + "sells.txt";
-	public static final String WINE_IMAGE_FILE = DB_FOLDER + "\\" + "wine_image.txt";
+	public static final String WINE_LISTINGS_FILE = DB_FOLDER + "\\" + "wine_listings.txt";
 	public static final String MESSAGE_FILE = DB_FOLDER + "\\" + "messages.txt";
-	public static final String STARTING_BALANCE = "200";
+	public static final int STARTING_BALANCE = 200;
 }
